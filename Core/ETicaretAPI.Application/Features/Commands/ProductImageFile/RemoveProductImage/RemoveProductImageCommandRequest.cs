8fc56aeedce61727d00630311ad6396fd6481c6b
qwe asd <aliklft61@outlook.com>
2022-06-02 22:49:56 +0300

    ...

---
+++ Core/ETicaretAPI.Application/Features/Commands/ProductImageFile/RemoveProductImage/RemoveProductImageCommandRequest.cs
@@ -0,0 +1,18 @@
+﻿
+using ETicaretAPI.Application.Features.Commands.ProductImageFile.RemoveImageFile;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.ProductImageFile.RemoveProductImage
+{
+    public class RemoveProductImageCommandRequest : IRequest<RemoveProductImageCommandResponse>
+    {
+        public string Id { get; set; }
+        public string? ImageId { get; set; }
+    }
+}
